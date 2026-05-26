f65af5b115b8af127106ef9aa68775e1d764da5c
Arisness <angeljavicueche@gmail.com>
2026-05-25 20:48:14 -0400

    refactor: changed updateUserValues to be a method

--- basicObjects/UserManagement.js
+++ basicObjects/UserManagement.js
@@ -69,7 +69,29 @@ class UserManagement
         const results = await runQuery([[queries.user.getUser, [username]]]);
         return results.rows;
     }
-    async updateValues(username, email, oldUsername) {await runQuery([[queries.user.updateValue, [username, email, oldUsername]]]);}
+    async updateUserValues(req, res){
+        if (sessionHandler.checkSession(req)){
+            const options = ['name', 'email'];
+            if(req.body.option == 0 || req.body.option == 1){
+                const userChecked = await userManagement.getUser(req.body.value);
+                if (userChecked.length > 0){
+                    return res.status(400).json({status: 'error', message: `User already exists with this ${req.body.option === 0 ? 'username' : 'email'}.`});
+                }
+            }
+            const query = queries.user.updateValue.replace('{{var}}', options[req.body.option]);
+            try{
+                await runQuery([[query, [req.body.value, req.session.user]]]);
+                if (req.body.option == 0) req.session.user = req.body.value;
+                return res.status(200).json({status: 'success', message: 'User information updated successfully.'});
+            }
+            catch (error)
+            {
+                logger.error(`Error updating user information: ${error}`);
+                return res.status(500).json({status: 'error', message: 'Internal server error while updating user information.'});
+            }
+        }
+        else return res.status(401).json({status: 'error', message: 'Unauthorized. Please log in to update your information.'});
+    }
 }
 
 const userManagement = new UserManagement();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -44,27 +44,7 @@ router.post('/reset-password', async (req, res) =>{
 });
 
 router.post('/update-user', async (req, res) =>{
-
-    if (sessionHandler.checkSession(req)){
-        const options = ['name', 'email'];
-        if(req.body.option == 0 || req.body.option == 1){
-            const userChecked = await userManagement.getUser(req.body.value);
-            if (userChecked.length > 0){
-                return res.status(400).json({status: 'error', message: 'User already exists with this email or username.'});
-            }
-        }
-        const query = queries.user.updateValue.replace('{{var}}', options[req.body.option]);
-        try{
-            await runQuery([[query, [req.body.value, req.session.user]]]);
-            return res.status(200).json({status: 'success', message: 'User information updated successfully.'});
-        }
-        catch (error)
-        {
-            logger.error(`Error updating user information: ${error}`);
-            return res.status(500).json({status: 'error', message: 'Internal server error while updating user information.'});
-        }
-    }
-    else return res.status(401).json({status: 'error', message: 'Unauthorized. Please log in to update your information.'});
+    return userManagement.updateUserValues(req, res);
 });
 
 export default router;
